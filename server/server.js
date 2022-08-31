0af900e6918858f640fb4b4dd31c7d77cbfb8531
Matt Gates <matthew.gates2001@gmail.com>
2022-08-31 12:59:49 -0600

    new css

--- server/server.js
+++ server/server.js
@@ -7,7 +7,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"))
 })
-
+app.get('/css', (req, res) => {
+    res.sendFile(path.join)(__dirname, "../client/index.html")
+})
 
 
 const port = process.env.PORT || 4005;
